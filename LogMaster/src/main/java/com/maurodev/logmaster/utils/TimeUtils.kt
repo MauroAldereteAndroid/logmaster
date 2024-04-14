8c576d037e456d176ba4ecec7d0c0f83be046d07
Mauro Alderete <131892094+MauroAldereteAndroid@users.noreply.github.com>
2024-04-14 10:31:10 -0300

    [Feature] Added repository and coroutine dependence

---
+++ LogMaster/src/main/java/com/maurodev/logmaster/utils/TimeUtils.kt
@@ -0,0 +1,18 @@
+package com.maurodev.logmaster.utils
+
+object TimeUtils {
+
+    /**
+     * This values are optionals for user.
+     */
+    const val LAST_15_MIN = "15min"
+    const val LAST_30_MIN = "30min"
+    const val LAST_1_HOUR = "1hour"
+    const val LAST_12_HOUR = "12hours"
+    const val DAY = "day"
+    const val WEEK = "week"
+    const val MONTH = "month"
+    const val ALL = "all"
+
+    // parsers and helpers here !
+}
